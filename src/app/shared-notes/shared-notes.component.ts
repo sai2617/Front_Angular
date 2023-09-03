440abf8aa0d6519b24b4e76c49fb2b539dd484e6
sai2617 <37062077+sai2617@users.noreply.github.com>
2023-09-04 02:09:17 +0530

    Sixth Commit

--- src/app/shared-notes/shared-notes.component.ts
+++ src/app/shared-notes/shared-notes.component.ts
@@ -7,8 +7,14 @@ interface Note {
   content: string;
   user: string;
   showContent: boolean;
-  editMode: boolean; // Add this property
+  editMode: boolean; 
   createdAt: Date;
+}
+
+interface Discussion {
+  title: string;
+  content: string;
+  showContent: boolean;
 }
 
 @Component({
@@ -17,49 +23,50 @@ interface Note {
   styleUrls: ['./shared-notes.component.css']
 })
 export class SharedNotesComponent {
+  employeename: string = ""; 
 
-  constructor( private router: Router) { }
+  constructor(private router: Router) { }
 
   filteredNotes: Note[] = [
     { id: 1, title: 'Physics', content: 'Shared Physics note.', user: 'sur', showContent: false, editMode: false, createdAt: new Date() },
     { id: 2, title: 'Chemistry', content: 'Shared Chemistry note.', user: 'Ma', showContent: false, editMode: false, createdAt: new Date() },
     { id: 3, title: 'Physics', content: 'Shared Physics note.', user: 'Sa', showContent: false, editMode: false, createdAt: new Date() },
     { id: 4, title: 'Chemistry', content: 'Shared Chemistry note.', user: 'Sha', showContent: false, editMode: false, createdAt: new Date() },
-    
     // Add more shared notes here...
   ];
-  sharedNotes: Note[] = [
-    { id: 1, title: 'Physics', content: 'Shared Physics note.', user: 'sur', showContent: false, editMode: false, createdAt: new Date() },
-    { id: 2, title: 'Chemistry', content: 'Shared Chemistry note.', user: 'Ma', showContent: false, editMode: false, createdAt: new Date() },
-    { id: 3, title: 'Physics', content: 'Shared Physics note.', user: 'Sa', showContent: false, editMode: false, createdAt: new Date() },
-    { id: 4, title: 'Chemistry', content: 'Shared Chemistry note.', user: 'Sha', showContent: false, editMode: false, createdAt: new Date() },
-    
-    // Add more shared notes here...
+
+  discussions: Discussion[] = [
+    { title: 'Physics', content: 'Content of discussions for Physics.', showContent: false },
+    { title: 'Mathematics', content: 'Content of discussion for Mathematics.', showContent: false },
+    // Add more discussion objects here...
   ];
 
   filterNotes(event: any): void {
     const searchTerm = event.target.value;
     if (searchTerm.trim() !== '') {
-      this.filteredNotes = this.sharedNotes.filter(note =>
+      this.filteredNotes = this.filteredNotes.filter(note =>
         note.title.toLowerCase().includes(searchTerm.toLowerCase())
       );
     } else {
-      this.filteredNotes = this.sharedNotes.slice(); 
+      this.filteredNotes = this.filteredNotes.slice();
     }
   }
 
   toggleNoteContent(note: Note): void {
     note.showContent = !note.showContent;
-    note.editMode = true; // Disable edit mode when toggling content
+    note.editMode = true; 
   }
 
   saveContent(note: Note): void {
     this.router.navigateByUrl('/notes-list');
-    note.editMode = true; 
+    note.editMode = true;
+  }
+
+  toggleDiscussion(discussion: Discussion): void {
+    discussion.showContent = !discussion.showContent;
   }
 
   logout(): void {
     this.router.navigateByUrl('/login');
   }
-
 }
